7066a66ee4dcf19cc395bc60dda0e0e78ea246bd
Abir <abirhossain9@iut-dhaka.edu>
2023-10-28 19:17:27 +0600

    Makes a simple rest controller

--- src/main/java/com/example/product_service/controller/HomeController.java
+++ src/main/java/com/example/product_service/controller/HomeController.java
@@ -7,13 +7,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Controller
+@RestController
 public class HomeController {
 
-    HomeService homeService;
+    private final HomeService homeService;
+
+    public HomeController(HomeService homeService) {
+        this.homeService = homeService;
+    }
 
     @GetMapping("/")
     public String home() {
